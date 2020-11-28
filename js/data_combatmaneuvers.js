8daf14d4cd7fcafe62de1ffc44ff938981c9ca72
Azahara <azaferna@ucm.es>
2020-11-29 00:46:50 +0100

    maniobras-combate: subido apartado de maniobras de combate

---
+++ js/data_combatmaneuvers.js
@@ -0,0 +1,74 @@
+data_combatmaneuvers = [
+    {
+        title: "Arrollar",
+        icon: "push",
+        subtitle: "Atraviesa la casilla de un enemigo.",
+        description: "Permite moverte a través de una casilla enemiga.",
+        reference: "PRB, pg. 199.",
+        bullets: [
+            "Como acción estándar, llevada a cabo durante tu movimiento, o como parte de una carga, puedes intentar arrollar a tu objetivo, moviéndote a través de su casilla. Sólo puedes arrollar a un oponente que no sea más de una categoría de tamaño más grande que tú. Iniciar un arrollamiento provoca un ataque de oportunidad por parte del objetivo de tu maniobra."
+        ]
+    },
+    {
+        title: "Derribar",
+        icon: "falling",
+        subtitle: "Empuja a un enemigo para derribarlo.",
+        description: "Tira a un enemigo al suelo.",
+        reference: "PRB, pg. 199.",
+        bullets: [
+            "En lugar de un ataque cuerpo a cuerpo, puedes intentar derribar a tu oponente. Sólo puedes derribar a un oponente que no sea de más de una categoría de tamaño mayor que tú. Si no dispones de la dote Derribo mejorado, o una aptitud similar, iniciar un derribo provoca un ataque de oportunidad por parte del objetivo de la maniobra"
+        ]
+    },
+    {
+        title: "Desarmar",
+        icon: "drop-weapon",
+        subtitle: "Quitar un objeto a un enemigo.",
+        description: "Quita a eleccion del jugador un objeto de las manos de un enemigo.",
+        reference: "PRB, pg. 199.",
+        bullets: [
+            "Puedes intentar desarmar a tu oponente en lugar de llevar a cabo un ataque cuerpo a cuerpo. Si no dispones de la dote Desarme mejorado o una aptitud similar, un intento de desarmar a un enemigo provoca un ataque de oportunidad por parte del objetivo de la maniobra. Intentar desarmar a un objetivo estando tú desarmado te impone un penalizador -4 al ataque"
+        ]
+    },
+    {
+        title: "Embestir",
+        icon: "punch",
+        subtitle: "Empujar a un enemigo hacia atrás.",
+        description: "Empuja a un enemigo hacia detrás.",
+        reference: "PRB, pg. 200.",
+        bullets: [
+            "Puedes llevar a cabo una acción de embestida como acción estándar o como parte de una carga,en lugar de un ataque cuerpo a cuerpo. Sólo puedes embestir a un oponente que no sea másde una categoría de tamaño mayor que tú. Un intento de embestir pretende echar atrása un oponente sin causarle daño alguno. Si no dispones de la dote Embestida mejorada o una aptitud similar,iniciar una embestida provoca un ataque de oportunidad por parte del objetivo de la maniobraSi tu ataque tiene éxito empujas hacia atrás al objetivo 5 pies (1,5 m).Por cada 5 puntos por los que tu ataque supera la DMC de tu oponente,puedes empujar hacia atrás al objetivo 5 pies (1,5 m) adicionales."
+        ]
+    },
+    {
+        title: "Fintar",
+        icon: "two-shadows",
+        subtitle: "Tratas de engañar al enemigo",
+        description: "Intentas una finta con una prueba de Engañar",
+        reference: "SFCR, pág. 246.",
+        bullets: [
+            "Usas tu acción estándar para intentar una finta con una prueba de Engañar, La CD es igual a 10 + el bonificador total de Averiguar intenciones del oponente o 15 + (1.5 x el VD del oponente), lo que sea mayor.",
+			"No puedes fintar contra una criatura que no tiene puntuación de Inteligencia y no puedes tomar 10 ni 20 en una prueba de engañar para fintar.",
+			"Cuando tienes éxito en una finta, tratas a tu oponente como desprevenido en tu siguiente ataque contra él antes del final de tu siguiente turno."
+        ]
+    },
+    {
+        title: "Presa",
+        icon: "grab",
+        subtitle: "Agarra a un enemigo.",
+        description: "Sujeta a un enemigo.",
+        reference: "PRB, pg. 200.",
+        bullets: [
+            "Como acción estándar, puedes intentar apresar a un enemigo, reduciendo sus opciones de combate. Si no dispones de Presa mejorada, agarrón, o una aptitud similar, intentar apresar a un objetivo provoca un ataque de oportunidad por parte del mismo. Si tienes éxito, tanto tu objetivo como tú pasáis al estado de apresado. Si consigues apresar con éxito a una criatura que no está adyacente a ti, muévela a un espacio adyacente libre (si no hay espacio disponible, tu presa falla). "
+        ]
+    },
+    {
+        title: "Romper Arma",
+        icon: "broken-axe",
+        subtitle: "Rompe el arma del un enemigo.",
+        description: "Sujeta a un enemigo",
+        reference: "PRB, pg. 200.",
+        bullets: [
+            "Puedes intentar romper un objeto que tu oponente empuña o lleva puesto como parte de una acción de ataque, en lugar de un ataque cuerpo a cuerpo. Si no dispones de la dote Romper arma mejorado, o  una aptitud similar, intentar romper un objeto provoca un ataque de oportunidad por parte del objetivo de la maniobra. Si tu ataque tiene éxito, infliges daño al objeto de la forma normal. Todo daño que excede la Dureza del objeto se resta de sus puntos de golpe. Si el objeto queda reducido a la mitad de sus puntos de golpe totales o menos, pasa al estado ‘roto’. Si el daño que infliges reduciría al objeto a menos de 0 puntos de golpe, puedes elegir destruirlo. Si eliges no destruirlo, el objetivo queda reducido a 1 solo punto de golpe y pasa al estado ‘roto’."
+        ]
+    },
+]
